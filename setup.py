462fbf3f6f6eb435c82c18b4fccdfa494b7fade0
Timothy Helton <timothy.j.helton@gmail.com>
2018-10-05 06:08:20 -0600

    MNT: add Pillow to package

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     keywords='EnterKeywordsHere',
     packages=find_packages(exclude=['docs', 'tests*']),
     install_requires=[
+        'Pillow',
         'numpy',
         'pytest',
     ],
