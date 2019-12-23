26839b6e5b1ef860808fdc00289c03cfb0c737d3
Timothy Helton <timothy.helton@semanticbits.com>
2019-12-23 07:26:47 -0700

    MNT: update package files

--- setup.py
+++ setup.py
@@ -1,19 +1,62 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from codecs import open
-import os.path as osp
+from pathlib import Path
+from operator import itemgetter
 import re
+from typing import Iterable, List, Union
 
 from setuptools import setup, find_packages
+
+
+dependencies = {
+    'build': {
+        'setuptools',
+        'wheel',
+    },
+    'docs': {
+        'sphinx',
+        'sphinx_rtd_theme',
+    },
+    'jupyter': {
+        'jupyter',
+        'jupyterlab',
+    },
+    'profile': {
+        'memory_profiler',
+        'snakeviz',
+    },
+    'test': {
+        'Faker',
+        'git-lint',
+        'pytest',
+        'pytest-cov',
+        'pytest-pep8',
+    },
+}
+
+
+def combine_dependencies(extras: Union[str, Iterable[str]]) -> List[str]:
+    """
+    Combine package dependencies.
+
+    :param extras: key(s) from the `dependencies` dictionary
+    :return: The minimum set of package dependencies contained in `extras`.
+    """
+    if isinstance(extras, str):
+        deps = set(itemgetter(extras)(dependencies))
+    else:
+        deps = set().union(*itemgetter(*extras)(dependencies))
+    return list(deps)
 
 
 with open('chrysalis/__init__.py', 'r') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         fd.read(), re.MULTILINE).group(1)
 
-here = osp.abspath(osp.dirname(__file__))
-with open(osp.join(here, 'README.md'), encoding='utf-8') as f:
+here = Path(__file__).absolute().parent
+with open(here / 'README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -31,23 +74,40 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Build Tools',
-    ],
+        ],
     keywords='EnterKeywordsHere',
-    packages=find_packages(exclude=['docs', 'tests*']),
+    packages=find_packages(exclude=[
+        'data',
+        'docker',
+        'docs',
+        'notebooks',
+        'wheels',
+        '*tests',
+        ]
+    ),
     install_requires=[
+        'click',
         'Pillow',
         'numpy',
-        'pytest',
     ],
     extras_require={
-        'tf_cpu': ['tensorflow'],
-        'tf_gpu': ['tensorflow-gpu'],
+         'all': combine_dependencies(dependencies.keys()),
+         'build': combine_dependencies(('build', 'test')),
+         'docs': combine_dependencies('docs'),
+         'jupyter': combine_dependencies('jupyter'),
+         'profile': combine_dependencies('profile'),
+         'test': combine_dependencies('test'),
     },
     package_dir={'chrysalis': 'chrysalis'},
     include_package_data=True,
-    )
+    entry_points={
+        'console_scripts': [
+            'count=chrysalis.cli:count',
+        ]
+    }
+)
 
 
 if __name__ == '__main__':
